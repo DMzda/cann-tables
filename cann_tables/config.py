1272b7f964545b2cbc61877f706aff948acc5b7a
Mazin Ibrahim <me@dmzda.co.uk>
2014-04-20 16:48:34 +0100

    Added Postgresql in deployment support

--- cann_tables/config.py
+++ cann_tables/config.py
@@ -1,4 +1,7 @@
 import os
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(APP_ROOT, "cann.db")
+if os.environ.get("CANN_ENV") == "prod":
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+else:
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(APP_ROOT, "cann.db")
