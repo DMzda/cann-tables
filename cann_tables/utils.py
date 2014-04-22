c575a8f69bd95d265e50413d0d35f8400bca8750
Mazin Ibrahim <me@dmzda.co.uk>
2014-04-22 17:09:21 +0100

    Started password handling

---
+++ cann_tables/utils.py
@@ -0,0 +1,20 @@
+#TODO: Implement a utility to allow the creation of a user through manage.py
+from cann_tables import db, models
+from passlib.context import CryptContext
+
+import getpass
+
+passwd_context = CryptContext(schemes=["pbkdf2_sha512"])
+
+
+def add_user():
+    """Add a user to the database"""
+    username = input("Username: ")
+    password = getpass.getpass()
+
+    pwd_hash = passwd_context.encrypt(password)
+
+    user = models.User(username=username, password=pwd_hash)
+    db.session.add(user)
+    db.session.commit()
+    print("User {} added".format(username))
--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ from flask_script import Manager, Server, Shell, Command
 from flask_migrate import MigrateCommand
 from flask_frozen import Freezer
 
-from cann_tables import app, db
+from cann_tables import app, db, utils
 from cann_tables.models import Team, League
 import table_scraper
 
@@ -13,11 +13,16 @@ def _make_context():
     return {"app": app, "db": db, "Team": Team, "League": League}
 
 
-class Scrape(Command):
+@manager.command
+def scrape():
     """Run scraper"""
+    table_scraper.scrape_all()
 
-    def run(self):
-        table_scraper.scrape_all()
+
+@manager.command
+def add_user():
+    """Add a user to the database"""
+    utils.add_user()
 
 
 class Freeze(Command):
@@ -30,7 +35,6 @@ class Freeze(Command):
 manager.add_command("db", MigrateCommand)
 manager.add_command("runserver", Server())
 manager.add_command("shell", Shell(make_context=_make_context))
-manager.add_command("scrape", Scrape())
 manager.add_command("freeze", Freeze())
 
 if __name__ == "__main__":
