1da54d18377dbd6de24bad54a72351c99696790f
Mazin Ibrahim <me@dmzda.co.uk>
2014-04-24 17:46:04 +0100

    Auto-hide checkboxes

--- cann_tables/static/js/script.js
+++ cann_tables/static/js/script.js
@@ -1,13 +1,21 @@
-$(document).ready(function () {
+$(document).ready(function(){
     //Hide the choices 
     $("#chooseExpand").click(function(){
         var html = $(this).html();
-        $("#choose").slideToggle(400, function() {
+        $("#choose").slideToggle(400, function(){
             if ($("#chooseExpand").text().indexOf("+") === -1) {
                 $("#chooseExpand").html(html.replace("-", "+"));
             } else {
                 $("#chooseExpand").html(html.replace("+", "-"));
             }
         });
+    });
+
+    //Hide if filtered
+    $.each($("#choose :checkbox"), function(){
+        if (this.checked) {
+            $("#chooseExpand").trigger("click");
+        }
+        return false;
     });
 });
