222ee3a7744313eb6e3b6c3484566dc194c4bba8
Mazin Ibrahim <me@dmzda.co.uk>
2014-04-27 21:41:26 +0100

    Add CSRF protection

--- cann_tables/__init__.py
+++ cann_tables/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_wtf.csrf import CsrfProtect
 
 app = Flask(__name__)
 app.config.from_object("cann_tables.config")
@@ -12,5 +13,7 @@ migrate = Migrate(app, db)
 login_manager = LoginManager(app)
 login_manager.login_view = "login"
 login_manager.login_message_category = "warning"
+
+csrf = CsrfProtect(app)
 
 from cann_tables import views, models
